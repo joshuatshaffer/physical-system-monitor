71b3b0bb860b9e2eb4b0e0d5e3ec6e2b369be8fb
Joshua <joshuatshaffer@gmail.com>
2015-10-16 21:58:50 -0500

    Switched to Rogue Codes SoftPWM.

--- system_monitor_arduino/system_monitor_arduino.ino
+++ system_monitor_arduino/system_monitor_arduino.ino
@@ -1,26 +1,14 @@
-#include "Spwm.h"
+#include <SoftPWM.h>
 #include "Hexinator.h"
 
-Spwm indicators[8] = {
-/*core 0*/Spwm(2),
-/*core 1*/Spwm(3),
-/*core 2*/Spwm(4),
-/*core 3*/Spwm(5),
-
-/*core 4*/Spwm(6),
-/*core 5*/Spwm(7),
-/*core 6*/Spwm(8),
-/*core 7*/Spwm(9)
-};
+uint8_t cpu_pins[8] = {2,3,4,5, 6,7,8,9};
 
 int memory_pin = 10;
 int av_cpu_pin = 11;
 
 void setup() {
   Serial.begin(9600);
-  for (int i=0; i<8; ++i) {
-    indicators[i].do_setup();
-  }
+  SoftPWMBegin();
   pinMode(memory_pin, OUTPUT);
   pinMode(av_cpu_pin, OUTPUT);
 }
@@ -39,21 +27,11 @@ void loop() {
       
       for (int i=0; i<8; ++i) {
         if (get_num(this_n)) {
-          indicators[i].set(this_n);
+          SoftPWMSet(cpu_pins[i], this_n);
         } else {
           break;
         }
       }
-      
-      av = 0;
-      for (int i=0; i<8; ++i) {
-        av += indicators[i].get();
-      }
-      analogWrite(av_cpu_pin, av / 8);
     }
-  }
-  now = micros() % PERIOD;
-  for (int i=0; i<8; ++i) {
-    indicators[i].do_loop(now);
   }
 }
