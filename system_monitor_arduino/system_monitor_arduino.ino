2fed13dd269dafdc22e3513cc3e7e95056debe98
Joshua <joshuatshaffer@gmail.com>
2015-10-17 00:02:51 -0500

    Finagaling with the file system. Dont mind me.

--- system_monitor_arduino/system_monitor_arduino.ino
+++ system_monitor_arduino/system_monitor_arduino.ino
@@ -1,40 +1,27 @@
-#include <SoftPWM.h>
+#include "SoftPWM.h"
 #include "Hexinator.h"
 
-uint8_t cpu_pins[8] = {2,3,4,5, 6,7,8,9};
-
-int memory_pin = 10;
-int av_cpu_pin = 11;
+uint8_t cpu_pins[10] = {10, 2,3,4,5, 6,7,8,9, 11};
 
 void setup() {
   Serial.begin(9600);
   SoftPWMBegin();
-  pinMode(memory_pin, OUTPUT);
-  pinMode(av_cpu_pin, OUTPUT);
+  for (int i=0; i<10; ++i)
+    SoftPWMSet(cpu_pins[i], 0);
 }
 
-
-int c, this_n, av;
-long now;
+int c, this_n;
 
 void loop() {
   if (Serial.available() > 20) {
     c = Serial.read();
     if (c == 'x') {
-      if (get_num(this_n)) {
-        analogWrite(memory_pin, this_n);
-      }
-      
-      for (int i=0; i<8; ++i) {
+      for (int i=0; i<10; ++i) {
         if (get_num(this_n)) {
           SoftPWMSet(cpu_pins[i], this_n);
         } else {
           break;
         }
-      }
-      
-      if (get_num(this_n)) {
-        analogWrite(av_cpu_pin, this_n);
       }
     }
   }
