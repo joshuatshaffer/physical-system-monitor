8837e7b9089766979d7a190ebc819273fb279f0c
Joshua T Shaffer <joshuatshaffer@gmail.com>
2015-10-19 11:37:23 -0500

    Renamed cpu_pins to pins to reflect more general use.

--- system_monitor_arduino/system_monitor_arduino.ino
+++ system_monitor_arduino/system_monitor_arduino.ino
@@ -1,13 +1,13 @@
 #include "SoftPWM.h"
 #include "Hexinator.h"
 
-uint8_t cpu_pins[10] = {10, 2,3,4,5, 6,7,8,9, 11};
+uint8_t pins[10] = {10, 2,3,4,5, 6,7,8,9, 11};
 
 void setup() {
   Serial.begin(9600);
   SoftPWMBegin();
   for (int i=0; i<10; ++i)
-    SoftPWMSet(cpu_pins[i], 0);
+    SoftPWMSet(pins[i], 0);
 }
 
 int c, this_n;
@@ -18,7 +18,7 @@ void loop() {
     if (c == 'x') {
       for (int i=0; i<10; ++i) {
         if (get_num(this_n)) {
-          SoftPWMSet(cpu_pins[i], this_n);
+          SoftPWMSet(pins[i], this_n);
         } else {
           break;
         }
