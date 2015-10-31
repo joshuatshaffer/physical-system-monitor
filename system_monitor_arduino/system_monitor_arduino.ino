cde86ebac6e4468b4c7bc5817e640814d982300f
Joshua <joshuatshaffer@gmail.com>
2015-10-31 00:02:20 -0500

    Cleaned up code a bit.

--- system_monitor_arduino/system_monitor_arduino.ino
+++ system_monitor_arduino/system_monitor_arduino.ino
@@ -1,28 +1,30 @@
 #include "SoftPWM.h"
 #include "Hexinator.h"
 
-uint8_t pins[10] = {10, 2,3,4,5, 6,7,8,9, 11};
+const uint8_t pins[10] = {10, 2,3,4,5, 6,7,8,9, 11};
+const long sleep_timeout = 5000;
 
 long sleep_at = 0;
-long sleep_timeout = 5000;
+
+void zero_out_pins() {
+  for (int i=0; i<10; ++i)
+    SoftPWMSet(pins[i], 0);
+}
 
 void setup() {
   Serial.begin(9600);
   SoftPWMBegin();
-  for (int i=0; i<10; ++i)
-    SoftPWMSet(pins[i], 0);
+  zero_out_pins();
   sleep_at = millis() + sleep_timeout;
 }
 
-int c, this_n;
-
 void loop() {
   if (Serial.available() > 20) {
-    c = Serial.read();
-    if (c == 'x') {
+    if ('x' == Serial.read()) {
       for (int i=0; i<10; ++i) {
-        if (get_num(this_n)) {
-          SoftPWMSet(pins[i], this_n);
+        int num;
+        if (get_num(num)) {
+          SoftPWMSet(pins[i], num);
         } else {
           break;
         }
@@ -30,8 +32,7 @@ void loop() {
       sleep_at = millis() + sleep_timeout;
     }
   } else if (sleep_at <= millis()) {
-      for (int i=0; i<10; ++i)
-        SoftPWMSet(pins[i], 0);
+      zero_out_pins();
       sleep_at = 0x7FFFFFFF;
   }
 }
