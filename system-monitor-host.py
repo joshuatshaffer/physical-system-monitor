c2fe81cdb9c87fdf6d4d7635b9f6cd69b9d8f53c
Joshua <joshuatshaffer@gmail.com>
2015-11-30 22:26:19 -0600

    added slowing to leds

--- system-monitor-host.py
+++ system-monitor-host.py
@@ -12,33 +12,34 @@ arduino_port_name = '/dev/tty.usbmodemfd141'
 baud_rate = 9600
 
 
+lastCpus = [0.0] * 8
+slew = 0.2
+
+
+def getInfos():
+    global lastCpus
+    cpus = psutil.cpu_percent(percpu=True)
+    lastCpus = [a*(1-slew) + b*slew for a,b in zip(lastCpus, cpus)]
+    out_info = list(lastCpus)
+    out_info.insert(0, psutil.virtual_memory().percent)
+    out_info.append(sum(cpus)/len(cpus))
+    return out_info
+
+
 def updateInfos(ser):
-    x = psutil.virtual_memory().percent
-    out_put = 'x' + format_lin(x)
-    sum_cpu = 0
-    for x in psutil.cpu_percent(percpu=True):
-        out_put += format_led(x)
-        sum_cpu += x
-    out_put += format_lin(sum_cpu / 8.0)
+    infos = getInfos()
+    out_put = 'x%s%s%s' % (format_lin(infos[0]), "".join([format_led(x) for x in infos[1:-1]]), format_lin(infos[-1]))
     print(out_put)  # for debugging
     ser.write(out_put)
 
 
 def main():
+    ser = serial.Serial(arduino_port_name, baud_rate)
+    print("we have connected")
+    time.sleep(2)
     while True:
-        try:
-            ser = serial.Serial(arduino_port_name, baud_rate)
-            print("we have connected")
-            time.sleep(2)
-            while True:
-                updateInfos(ser)
-                time.sleep(0.05)
-        except serial.SerialException:
-            print("SerialException received. Retrying in 5 seconds.")
-            time.sleep(5)
-        except:
-            print("IDK what happened. Retrying in 5 seconds.")
-            time.sleep(5)
+        updateInfos(ser)
+        time.sleep(0.05)
 
 if __name__ == '__main__':
     sys.exit(main())
