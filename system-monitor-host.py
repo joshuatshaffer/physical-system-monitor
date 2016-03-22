52ddcd93749155bdd70a6c3b0da7cc2a7b5a8842
Joshua Shaffer <joshuatshaffer@gmail.com>
2016-03-21 22:40:46 -0500

    converted to daemon

--- system-monitor-host.py
+++ system-monitor-host.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+
+from daemon3x import Daemon
+
 import psutil
 import serial
 
-import time
 import math
+import sys
+import time
 
 led_gamma = 2.2
 port_name = '/dev/tty.usbmodemFA131'
 baud_rate = 9600
 
-lcpu = 0
 
 def format_led(x):
     n = int(math.pow(x / 100.0, led_gamma) * 256.0)
@@ -31,23 +35,39 @@ def format_lin(x):
 def update_monitor(ser):
     cpus = psutil.cpu_percent(percpu=True)
     ram = psutil.virtual_memory().percent
-    tcpu = sum(cpus)/len(cpus)
+    tcpu = sum(cpus) / len(cpus)
     out_put = 'x%s%s%s' % (format_lin(ram), "".join([format_led(x) for x in cpus]), format_lin(tcpu))
     print(out_put)  # for debugging
     ser.write(out_put)
 
 
-def main():
-    while True:
-        try:
-            ser = serial.Serial(port_name, baud_rate)
-            print("Connected. Streaming data.")
-            while True:
-                update_monitor(ser)
-                time.sleep(0.1)
-        except BaseException as e:
-            print(e)
-        time.sleep(2)
+class ThisDaemon(Daemon):
+    def run(self):
+        while True:
+            try:
+                ser = serial.Serial(port_name, baud_rate)
+                print("Connected. Streaming data.")
+                while True:
+                    update_monitor(ser)
+                    time.sleep(0.1)
+            except BaseException as e:
+                print(e)
+            time.sleep(2)
 
 
-main()
+if __name__ == "__main__":
+    daemon = ThisDaemon('/tmp/pysical_system_monitor.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print("Unknown command")
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print("usage: %s start|stop|restart" % sys.argv[0])
+        sys.exit(2)
