d2f1e65d144e5911abf0961d9404e70e0069d525
Joshua Shaffer <joshuatshaffer@gmail.com>
2016-08-04 15:56:33 -0500

    Reimplemented daemonizing.

--- host/retro_cpu_monitor_host.py
+++ host/retro_cpu_monitor_host.py
@@ -3,9 +3,7 @@
 import psutil # v3.2.2 by Giampaolo Rodola
 import serial # v2.7 by Chris Liechti
 
-import math
-import sys
-import time
+import atexit, math, os, signal, sys, time
 
 led_gamma = 2.2
 port_name = '/dev/tty.wchusbserial1420'
@@ -102,10 +100,113 @@ def main(testing):
         time.sleep(1)
         print("Retrying...")
 
+        
+# ----- Daemon Stuff - based on "A simple unix/linux daemon in Python" by Sander Marechal -----
+        
+def delete_pid_file():
+    os.remove(pid_file)
+    
 
+def daemonize():
+    """Become a daemon."""
+
+    # Check for a pid file to see if the daemon is already runing
+    try:
+        with open(pid_file, 'r') as pf:
+            pid = int(pf.read().strip())
+    except IOError:
+        pid = None
+
+    if pid:
+        message = "{0} already exists. The daemon is already running.\n"
+        sys.stderr.write(message.format(pid_file))
+        sys.exit(1)
+
+    """Do a UNIX double fork thing."""
+
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # exit first parent
+            sys.exit(0)
+    except OSError as err:
+        sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+        sys.exit(1)
+
+    # decouple from parent environment
+    os.chdir('/')
+    os.setsid()
+    os.umask(0)
+
+    # do second fork
+    try:
+        pid = os.fork()
+        if pid > 0:
+            # exit from second parent
+            sys.exit(0)
+    except OSError as err:
+        sys.stderr.write('fork #2 failed: {0}\n'.format(err))
+        sys.exit(1)
+
+    # redirect standard file descriptors
+    sys.stdout.flush()
+    sys.stderr.flush()
+    si = open(os.devnull, 'r')
+    so = open(os.devnull, 'a+')
+    se = open(os.devnull, 'a+')
+
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+
+    # write pidfile
+    atexit.register(delete_pid_file)
+
+    pid = str(os.getpid())
+    with open(pid_file, 'w+') as f:
+        f.write(pid + '\n')
+
+    
+def stop_daemon():
+    """Stop the daemon."""
+
+    # Get the pid from the pid file
+    try:
+        with open(pid_file, 'r') as pf:
+            pid = int(pf.read().strip())
+    except IOError:
+        pid = None
+
+    if not pid:
+        message = "{0} does not exist. The daemon is not running.\n"
+        sys.stderr.write(message.format(pid_file))
+        return  # not an error in a restart
+
+    # Try killing the daemon process
+    try:
+        while 1:
+            os.kill(pid, signal.SIGTERM)
+            time.sleep(0.1)
+    except OSError as err:
+        e = str(err.args)
+        if e.find("No such process") > 0:
+            if os.path.exists(pid_file):
+                os.remove(pid_file)
+        else:
+            print(str(err.args))
+            sys.exit(1)     
+
+        
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
+            daemonize()
+            main(testing=False)
+        elif 'stop' == sys.argv[1]:
+            stop_daemon()
+        elif 'restart' == sys.argv[1]:
+            stop_daemon()
+            daemonize()
             main(testing=False)
         elif 'test' == sys.argv[1]:
             main(testing=True)
@@ -114,5 +215,5 @@ if __name__ == "__main__":
             sys.exit(2)
         sys.exit(0)
     else:
-        print("usage: %s start|test" % sys.argv[0])
+        print("usage: %s start|stop|test" % sys.argv[0])
         sys.exit(2)
