42fdd340118da3fc3a769ecdbbeef8fddc7f5a70
joshuatshaffer <joshuatshaffer@gmail.com>
2016-12-29 22:53:56 -0600

    Finished memory load meassuring.

--- host-haskell/Main.hs
+++ host-haskell/Main.hs
@@ -1,15 +1,21 @@
 module Main where
-import           Control.Arrow      ((&&&))
 import           Control.Concurrent (threadDelay)
-import           Data.List          (intercalate, isPrefixOf, genericIndex)
+import           Data.List          (find, genericIndex, isPrefixOf)
+import           Data.Maybe         (fromJust)
 import           System.IO          (hFlush, stdout)
+
+-- Basic Functions --
 
 mapT :: (a -> b) -> (a, a) -> [b]
 mapT f (a, b) = [f a, f b]
 
-cpuTimesFieldNames :: [String]
-cpuTimesFieldNames = ["user", "nice", "system", "idle", "iowait",
-                      "irq", "softirq", "steal", "guest", "guest_nice"]
+byte2Hex :: Integer -> String
+byte2Hex n = mapT (genericIndex "0123456789ABCDEF") $ quotRem n 16
+
+percentByte :: Integer -> Integer -> Integer
+percentByte p h = p * 255 `quot` h
+
+-- Data Fetching --
 
 getCpuTimes :: IO [(Integer, Integer)]
 getCpuTimes = do s <- readFile "/proc/stat"
@@ -19,29 +25,46 @@ getCpuTimes = do s <- readFile "/proc/stat"
     parceCpuTimes = map parceCpuLine . takeWhile (isPrefixOf "cpu") . lines
 
     parceCpuLine :: String -> (Integer, Integer)
-    parceCpuLine = (cpuTimeBusy &&& cpuTimeTot) . map read . tail . words
+    parceCpuLine l = (b, t)
+      where
+        times = map read . tail $ words l
+        t = sum $ take 8 times
+        b = sum $ take 3 times ++ take 3 (drop 5 times)
 
-    cpuTimeTot :: [Integer] -> Integer
-    cpuTimeTot = sum . take 8
-
-    cpuTimeBusy :: [Integer] -> Integer
-    cpuTimeBusy ts = sum $ take 3 ts ++ take 3 (drop 5 ts)
-
-levelsMessage :: [(Integer, Integer)] -> [(Integer, Integer)] -> String
-levelsMessage is fs = 'x' : concat (zipWith (\i f -> byte2Hex $ percentByte i f) is fs)
+showCpuLoad :: [(Integer, Integer)] -> [(Integer, Integer)] -> String
+showCpuLoad is fs = concat (zipWith (\i f -> byte2Hex $ usagePercent i f) is fs)
   where
-    percentByte :: (Integer, Integer) -> (Integer, Integer) -> Integer
-    percentByte i f = (fst f - fst i) * 255 `quot` (snd f - snd i)
+    usagePercent :: (Integer, Integer) -> (Integer, Integer) -> Integer
+    usagePercent i f = percentByte (fst f - fst i) (snd f - snd i)
 
-    byte2Hex :: Integer -> String
-    byte2Hex n = mapT (genericIndex "0123456789ABCDEF") $ quotRem n 16
+getMemLoad :: IO String
+getMemLoad = do s <- readFile "/proc/meminfo"
+                return $ parceMeminfo s
+  where
+    parceMeminfo :: String -> String
+    parceMeminfo s = byte2Hex $ usagePercent (a, t)
+      where
+        ls = lines s
+        a = parceMemLine $ find (isPrefixOf "MemAvailable") ls
+        t = parceMemLine $ find (isPrefixOf "MemTotal") ls
+
+    parceMemLine :: Maybe String -> Integer
+    parceMemLine l = read $ (!! 1) $ words $ fromJust l
+
+    usagePercent :: (Integer, Integer) -> Integer
+    usagePercent (a, t) = percentByte (t - a) t
+
+-- Execution and Control Flow --
 
 mainLoop :: [(Integer, Integer)] -> IO ()
 mainLoop lastTimes = do
-  threadDelay 100000
+  threadDelay 200000
   times <- getCpuTimes
+  memload <- getMemLoad
   putChar '\r'
-  putStr $ levelsMessage times lastTimes
+  putChar 'x'
+  putStr $ showCpuLoad times lastTimes
+  putStr memload
   hFlush stdout
   mainLoop times
 
