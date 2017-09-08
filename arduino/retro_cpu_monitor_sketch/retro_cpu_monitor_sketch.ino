6f8a40017324ccbb487ff90ff8b976ef737ec991
joshuatshaffer <joshuatshaffer@gmai.com>
2017-09-08 13:13:19 -0500

    I don't remember making these changes.

--- arduino/retro_cpu_monitor_sketch/retro_cpu_monitor_sketch.ino
+++ arduino/retro_cpu_monitor_sketch/retro_cpu_monitor_sketch.ino
@@ -12,7 +12,7 @@ int digitVal (const int& d) {
  switch (d) {
     case '0': return 0;
     case '1': return 1;
-    case '2': return 2;
+    case '2': return 2;  
     case '3': return 3;
     case '4': return 4;
     case '5': return 5;
--- host-haskell/Main.hs
+++ host-haskell/Main.hs
@@ -1,121 +1,112 @@
-module Main where
+module Main (main) where
 import           Control.Concurrent (threadDelay)
 import           Data.List          (find, genericIndex, isPrefixOf, replicate)
 import           Data.Maybe         (fromJust)
 import           System.Environment (getArgs)
-import           System.IO          (Handle, hFlush, hPutStr, stdout)
+import           System.IO          (Handle, hFlush, hPutStr, stdout, openFile, IOMode(WriteMode))
 import           System.Serial
+
+type Times = (Integer, Integer)
 
 -- Basic Functions --
 
-mapT :: (a -> b) -> (a, a) -> [b]
-mapT f (a, b) = [f a, f b]
+tupleSubtract (a,b) (c,d) = (a - c, b - d)
 
 byte2Hex :: Integer -> String
 byte2Hex n = mapT (genericIndex "0123456789ABCDEF") $ quotRem n 16
+  where mapT f (a, b) = [f a, f b]
 
-float2Hex :: RealFrac a => a -> String
-float2Hex = byte2Hex . round . (* 255)
+float2Times :: RealFrac a => a -> Times
+float2Times x = (round (x * 1024), 1024)
 
-percentByte :: Integer -> Integer -> Integer
-percentByte p h = p * 255 `quot` h
+percent2Hex :: Times -> String
+percent2Hex t = byte2Hex $ fst t * 255 `quot` snd t
 
-percentLedByte :: Integer -> Integer -> Integer
-percentLedByte p h = round ((fromInteger p / fromInteger h) ** 2.2 * 255.0) + 1
+percent2GammaHex :: Times -> String
+percent2GammaHex t = byte2Hex $ round ((fromInteger (fst t) / fromInteger (snd t)) ** 2.2 * 255.0) + 1
 
 -- Data Fetching --
 
-getCpuTimes :: IO [(Integer, Integer)]
+getCpuTimes :: IO [Times]
 getCpuTimes = do s <- readFile "/proc/stat"
-                 return $! parceCpuTimes s
+                 return $! parce s
   where
-    parceCpuTimes :: String -> [(Integer, Integer)]
-    parceCpuTimes = map parceCpuLine . takeWhile (isPrefixOf "cpu") . lines
-
-    parceCpuLine :: String -> (Integer, Integer)
-    parceCpuLine l = (b, t)
+    parce = map parce' . takeWhile (isPrefixOf "cpu") . lines
+    parce' l = (b, t)
       where
         times = map read . tail $ words l
         t = sum $ take 8 times
         b = sum $ take 3 times ++ take 3 (drop 5 times)
 
-showCpuLoad :: [(Integer, Integer)] -> [(Integer, Integer)] -> String
-showCpuLoad (ix:is) (fx:fs) = concat ((byte2Hex $ usagePercent ix fx) : zipWith (\i f -> byte2Hex $ usageLedPercent i f) is fs)
-  where
-    usagePercent :: (Integer, Integer) -> (Integer, Integer) -> Integer
-    usagePercent i f = percentByte (fst f - fst i) (snd f - snd i)
-    usageLedPercent :: (Integer, Integer) -> (Integer, Integer) -> Integer
-    usageLedPercent i f = percentLedByte (fst f - fst i) (snd f - snd i)
-
-getMemLoad :: IO String
+getMemLoad :: IO Times
 getMemLoad = do s <- readFile "/proc/meminfo"
-                return $ parceMeminfo s
+                return $! parse s
   where
-    parceMeminfo :: String -> String
-    parceMeminfo s = byte2Hex $ usagePercent (a, t)
-      where
-        ls = lines s
-        a = parceMemLine $ find (isPrefixOf "MemAvailable") ls
-        t = parceMemLine $ find (isPrefixOf "MemTotal") ls
+    parse s = (t - a, t)
+      where ls = lines s
+            a = parce' "MemAvailable" ls
+            t = parce' "MemTotal" ls
+    parce' a = read . (!! 1) . words . fromJust . find (isPrefixOf a)
 
-    parceMemLine :: Maybe String -> Integer
-    parceMemLine l = read $ (!! 1) $ words $ fromJust l
-
-    usagePercent :: (Integer, Integer) -> Integer
-    usagePercent (a, t) = percentByte (t - a) t
-
-showUpdate :: [(Integer, Integer)] -> [(Integer, Integer)] -> String -> String
-showUpdate times lastTimes memload = 'x' : showCpuLoad times lastTimes ++ memload
-
-triangleWave :: RealFrac a => Integer -> a -> String
-triangleWave s f | s == 0 = float2Hex f
-                 | s == 1 = "FF"
-                 | s == 2 = float2Hex (1 - f)
-                 | otherwise = "00"
+triangleWave :: (RealFrac a) => a -> a
+triangleWave x | mx < 1 = mx
+               | mx < 2 = 1
+               | mx < 3 = 3 - mx
+               | otherwise = 0
+              where mx = x `fmod` 4
+                    fmod a b | a < b = a
+                             | otherwise = fmod (a-b) b
 
 -- Execution and Control Flow --
 
-pushUpdate :: Handle -> String -> IO ()
+pushUpdate :: Handle -> [Times] -> IO ()
 pushUpdate ser update = do
-  putChar '\r'
-  putStr update
+  putStr $ '\r':s
   hFlush stdout
 
-  hPutStr ser update
+  hPutStr ser s
   hFlush ser
+    where
+      s = 'x' : concat (zipWith ($) (m:m:repeat l) update)
+      m = percent2Hex
+      l = percent2GammaHex
 
-testLoop :: RealFrac a => Handle -> Integer -> a -> IO ()
-testLoop ser s f = do
+
+doTest :: Handle -> IO ()
+doTest ser = testLoop ser 0
+
+testLoop :: (RealFrac a) => Handle -> a -> IO ()
+testLoop ser t = do
   threadDelay 100000
-  let (ns,nf) = if (f + 0.1) > 1 then (if s == 4 then 0 else s + 1, f + 0.1 - 1) else (s, f + 0.1)
-  let update = ('x':) . concat . replicate 6 $ triangleWave ns nf
+  let nt = t + 0.1
+  let update = replicate 6 . float2Times $ triangleWave nt
 
   pushUpdate ser update
 
-  testLoop ser ns nf
+  testLoop ser nt
 
-doTest :: Handle -> IO ()
-doTest ser = testLoop ser 0 0
 
-mainLoop :: Handle -> [(Integer, Integer)] -> IO ()
+doMain :: Handle -> IO ()
+doMain ser = getCpuTimes >>= mainLoop ser
+
+mainLoop :: Handle -> [Times] -> IO ()
 mainLoop ser lastTimes = do
   threadDelay 100000
   times <- getCpuTimes
   memload <- getMemLoad
-  let update = showUpdate times lastTimes memload
+  let update = memload : zipWith tupleSubtract times lastTimes
 
   pushUpdate ser update
 
   mainLoop ser times
 
-doMain :: Handle -> IO ()
-doMain ser = getCpuTimes >>= mainLoop ser
 
 main :: IO ()
 main = do
   (mode:portName:_) <- getArgs
 
-  ser <- openSerial portName B9600 8 One Even Software
+  --ser <- openSerial portName B9600 8 One Even Software
+  ser <- openFile "/dev/null" WriteMode
 
   case mode of
     "test" -> doTest ser
